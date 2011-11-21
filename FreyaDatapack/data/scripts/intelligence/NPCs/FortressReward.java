829f2f6e4519bea1d89a2e800bd5d16ed2961ff8
SomeDays <SomeDays@8c380beb-a0af-4294-b7aa-acb47a1333cf>
2011-11-21 14:43:13 +0000

    - Fix fortress script now only attackers of fortress can farm knight epaullete - Added skill for Rune of Fearther

--- FreyaDatapack/data/scripts/intelligence/NPCs/FortressReward.java
+++ FreyaDatapack/data/scripts/intelligence/NPCs/FortressReward.java
@@ -25,7 +25,10 @@ import l2.brick.gameserver.model.quest.Quest;
 import l2.brick.gameserver.model.quest.QuestState;
 import l2.brick.gameserver.util.Util;
 import l2.brick.util.Rnd;
-
+/*
+ * @author Unknown
+ * Updated By SomeDays
+ */
 public class FortressReward extends Quest
 {
 	private static final int KE = 9912;
@@ -38,30 +41,44 @@ public class FortressReward extends Quest
 	private static final int[] CGUARD_DROP    = { 2, 6, 66 };
 	private static final int[] DECORATED      = { 35909, 35872, 35671, 35703, 35809, 36154, 36048, 36086, 35840, 36123, 36331, 35979, 36016, 35740, 36369, 35941, 35772, 36224, 36262, 36299, 36186 };
 	private static final int[] DECORATED_DROP = { 2, 8, 78 };
-	private static final int[] DGSERG         = { 35711, 35814, 36159, 36056, 36094, 35845, 36128, 36339, 35949, 35987, 36021, 35745, 36377, 35780, 36232, 36270, 36304, 36194, 35914, 35880, 35676 };
-	private static final int[] DGSERG_DROP    = { 2, 6, 60 };
+	private static final int[] DGSERG         = { 35814, 36159, 35845, 36128, 36021, 35745, 36304, 35914, 35676 };
+	private static final int[] DGSERG_DROP    = { 2, 6, 61 };
+	private static final int[] SNOOP          = { 35711, 36056, 36094, 36339, 35949, 35987, 36377, 35780, 36232, 36270, 36194, 35880 };
+	private static final int[] SNOOP_DROP     = { 12, 36, 2 };
 	private static final int[] GENERAL        = { 36066, 36349, 35959, 35997, 36387, 35790, 36204, 36242, 36280, 35890, 35721 };
-	private static final int[] GENERAL_DROP   = { 20, 70, 70 };	
-	private static final int[] GUARD          = { 35944, 36198, 35982, 36236, 36019, 36274, 35743, 36372, 35848, 35884, 35775, 36131, 36227, 36265, 36302, 36189, 36024, 35715, 35748, 35912, 35875, 35674, 36060, 36098, 35706, 36307, 35812, 36157, 35917, 36051, 36089, 36343, 35843, 35953, 35991, 35679, 36381, 35784, 36126, 35817, 36162, 36334 };
-	private static final int[] GUARD_DROP     = { 2, 6, 60 };
+	private static final int[] GENERAL_DROP   = { 20, 77, 70 };	
+	private static final int[] GUARD          = { 35944, 35982, 36019, 35743, 36372, 35775, 36227, 36265, 36302, 36189, 35912, 35875, 35674, 35706, 35812, 36157, 36051, 36089, 35843, 36126, 36334 };
+	private static final int[] GUARD_DROP     = { 2, 6, 66 };
+	private static final int[] EVILUS         = { 36198,  36236, 36274, 35848, 35884, 36131, 35784, 35679, 36098, 36060, 36162, 35817, 36307, 36343, 36381, 36162, 35953, 35917, 35991, 35748, 35715, 36024 };
+	private static final int[] EVILUS_DROP    = { 2, 8, 73 };
 	private static final int[] GUARDCAP       = { 36058, 36096, 36305, 35915, 36341, 35951, 35989, 35677, 36379, 35782, 35815, 36160, 36196, 36234, 36272, 35846, 35882, 36129, 36022, 35713, 35746 };
-	private static final int[] GUARDCAP_DROP  = { 70, 150, 80 };
+	private static final int[] GUARDCAP_DROP  = { 70, 150, 84 };
 	private static final int[] HEALER         = { 36346, 35956, 35994, 35682, 36384, 35787, 35820, 36165, 35851, 36201, 36239, 36277, 35887, 36134, 36027, 35718, 35751, 36063, 36101, 36310, 35920 };
-	private static final int[] HEALER_DROP    = { 2, 8, 80 };
+	private static final int[] HEALER_DROP    = { 2, 8, 82 };
 	private static final int[] MINISTER       = { 36205, 36243, 36281, 35891, 35722, 36067, 36350, 35960, 35998, 36388, 35791 };
-	private static final int[] MINISTER_DROP  = { 20, 70, 70 };	
+	private static final int[] MINISTER_DROP  = { 20, 77, 70 };	
 	private static final int[] REBELCOM       = { 36122, 36330, 35978, 36015, 35739, 36368, 35940, 35771, 36223, 36261, 36298, 36185, 35908, 35871, 35670, 35702, 35808, 36153, 36047, 36085, 35839 };
 	private static final int[] REBELCOM_DROP  = { 20, 100, 90 };
 	private static final int[] REBELPRIV      = { 36332, 35980, 36017, 35741, 36370, 35942, 35773, 36225, 36263, 36300, 36187, 35910, 35873, 35672, 35704, 35810, 36155, 36049, 36087, 35841, 36124 };
-	private static final int[] REBELPRIV_DROP = { 2, 6, 70 };
+	private static final int[] REBELPRIV_DROP = { 2, 6, 72 };
 	private static final int[] SGOLEM         = { 35781, 36233, 36271, 36195, 35881, 35712, 36057, 36095, 36340, 35950, 35988, 36378 };
-	private static final int[] SGOLEM_DROP    = { 2, 3, 40 };
-	private static final int[] SBOX           = { 35665, 35697, 35803, 35834, 35734, 35766 };
-	private static final int[] SBOX_DROP      = { 60, 120, 100 };
+	private static final int[] SGOLEM_DROP    = { 2, 3, 46 };
+	private static final int[] SBOX           = { 35665 };    
+	private static final int[] SBOX_DROP      = { 20, 40, 100 };
+	private static final int[] COURT          = { 35834 };
+	private static final int[] COURT_DROP     = { 120, 240, 100 };
+	private static final int[] FORT           = { 35803 };
+	private static final int[] FORT_DROP      = { 100, 200, 100 };
+	private static final int[] BOMB           = { 35766 };
+	private static final int[] BOMB_DROP      = { 80, 160, 100 };
+	private static final int[] BOOM           = { 35734 };
+	private static final int[] BOOM_DROP      = { 60, 120, 100 };
+	private static final int[] GANG           = { 35697 };
+	private static final int[] GANG_DROP      = { 40, 80, 100 };
 	private static final int[] SUPPORT        = { 36308, 35918, 36344, 35954, 35992, 35680, 36382, 35785, 35818, 36163, 36199, 36237, 36275, 35849, 35885, 36132, 36025, 35716, 35749, 36061, 36099 };
-	private static final int[] SUPPORT_DROP   = { 70, 150, 80 };
+	private static final int[] SUPPORT_DROP   = { 70, 150, 84 };
 	private static final int[] WIZARD         = { 36133, 36026, 35717, 35750, 36062, 36100, 36309, 35919, 36345, 35955, 35993, 35681, 36383, 35786, 35819, 36164, 35850, 36200, 36238, 36276, 35886 };
-	private static final int[] WIZARD_DROP    = { 2, 8, 70 };
+	private static final int[] WIZARD_DROP    = { 2, 8, 77 };
 	
 	@Override
 	public final String onKill(L2Npc npc, L2PcInstance player, boolean isPet)
@@ -81,38 +98,52 @@ public class FortressReward extends Quest
 			FortSiege siege = fort.getSiege();
 			if(siege != null && siege.checkIsAttacker(clan)) 
 			{
-					if(Util.contains(CGUARD, npcid))
-						rewardPlayer(player, npc, CGUARD_DROP);
-	 				else if (Util.contains(GUARD, npcid))
-  						rewardPlayer(player, npc, GUARD_DROP);
-					else if (Util.contains(ARCHER, npcid))
- 						rewardPlayer(player, npc, ARCHER_DROP);
- 					else if (Util.contains(HEALER, npcid))
-						rewardPlayer(player, npc, HEALER_DROP);
-					else if (Util.contains(WIZARD, npcid))
-						rewardPlayer(player, npc, WIZARD_DROP);
-	                else if (Util.contains(SUPPORT, npcid))
-	                	rewardPlayer(player, npc, SUPPORT_DROP);
-	                else if (Util.contains(ARCHCAP, npcid))
-	                	rewardPlayer(player, npc, ARCHCAP_DROP);
- 	                else if (Util.contains(GUARDCAP, npcid))
- 	                	rewardPlayer(player, npc, GUARDCAP_DROP);
-	                else if (Util.contains(REBELPRIV, npcid))
-	                	rewardPlayer(player, npc, REBELPRIV_DROP);
-	                else if (Util.contains(DECORATED, npcid))
-	                	rewardPlayer(player, npc, DECORATED_DROP);
-	                else if (Util.contains(REBELCOM, npcid))
-	                	rewardPlayer(player, npc, REBELCOM_DROP);
-	                else if (Util.contains(DGSERG, npcid))
-	                	rewardPlayer(player, npc, DGSERG_DROP);	
-	                else if (Util.contains(GENERAL, npcid))
-	                	rewardPlayer(player, npc, GENERAL_DROP);
-	                else if (Util.contains(MINISTER, npcid))
-	                	rewardPlayer(player, npc, MINISTER_DROP);
-	                else if (Util.contains(SGOLEM, npcid))
-	                	rewardPlayer(player, npc, SGOLEM_DROP);
-	                else if (Util.contains(SBOX, npcid))
-	                	rewardPlayer(player, npc, SBOX_DROP);
+			if(Util.contains(CGUARD, npcid))
+				rewardPlayer(player, npc, CGUARD_DROP);
+	 		else if (Util.contains(GUARD, npcid))
+  				rewardPlayer(player, npc, GUARD_DROP);
+			else if (Util.contains(ARCHER, npcid))
+ 				rewardPlayer(player, npc, ARCHER_DROP);
+ 			else if (Util.contains(HEALER, npcid))
+				rewardPlayer(player, npc, HEALER_DROP);
+			else if (Util.contains(WIZARD, npcid))
+				rewardPlayer(player, npc, WIZARD_DROP);
+	        else if (Util.contains(SUPPORT, npcid))
+	            rewardPlayer(player, npc, SUPPORT_DROP);
+	        else if (Util.contains(ARCHCAP, npcid))
+	            rewardPlayer(player, npc, ARCHCAP_DROP);
+ 	        else if (Util.contains(GUARDCAP, npcid))
+ 	            rewardPlayer(player, npc, GUARDCAP_DROP);
+	        else if (Util.contains(REBELPRIV, npcid))
+	            rewardPlayer(player, npc, REBELPRIV_DROP);
+	        else if (Util.contains(DECORATED, npcid))
+	            rewardPlayer(player, npc, DECORATED_DROP);
+	        else if (Util.contains(REBELCOM, npcid))
+	            rewardPlayer(player, npc, REBELCOM_DROP);
+	        else if (Util.contains(DGSERG, npcid))
+	            rewardPlayer(player, npc, DGSERG_DROP);	
+	        else if (Util.contains(GENERAL, npcid))
+	            rewardPlayer(player, npc, GENERAL_DROP);
+	        else if (Util.contains(MINISTER, npcid))
+	            rewardPlayer(player, npc, MINISTER_DROP);
+	        else if (Util.contains(SGOLEM, npcid))
+	            rewardPlayer(player, npc, SGOLEM_DROP);
+	        else if (Util.contains(SBOX, npcid))
+	            rewardPlayer(player, npc, SBOX_DROP);
+			else if (Util.contains(SNOOP, npcid))
+	            rewardPlayer(player, npc, SNOOP_DROP);
+			else if (Util.contains(EVILUS, npcid))
+	            rewardPlayer(player, npc, EVILUS_DROP);
+			else if (Util.contains(COURT, npcid))
+	            rewardPlayer(player, npc, COURT_DROP);
+			else if (Util.contains(FORT, npcid))
+	            rewardPlayer(player, npc, FORT_DROP);
+			else if (Util.contains(BOMB, npcid))
+	            rewardPlayer(player, npc, BOMB_DROP);
+			else if (Util.contains(BOOM, npcid))
+	            rewardPlayer(player, npc, BOOM_DROP);	
+			else if (Util.contains(GANG, npcid))
+	            rewardPlayer(player, npc, GANG_DROP);
 			}
 		}
 		return super.onKill(npc, player, isPet);
@@ -172,6 +203,20 @@ public class FortressReward extends Quest
 			addKillId(golem);
 		for (int sbox : SBOX)
 			addKillId(sbox);
+	    for (int snoop : SNOOP)
+			addKillId(snoop);
+	    for (int evilus : EVILUS)
+			addKillId(evilus);
+	    for (int court : COURT)
+			addKillId(court);
+	    for (int fort : FORT)
+			addKillId(fort);
+	    for (int bomb : BOMB)
+			addKillId(bomb);
+	    for (int boom : BOOM)
+			addKillId(boom);
+	    for (int gang : GANG)
+			addKillId(gang);
 	}
 	
 	public static void main(String[] args)
