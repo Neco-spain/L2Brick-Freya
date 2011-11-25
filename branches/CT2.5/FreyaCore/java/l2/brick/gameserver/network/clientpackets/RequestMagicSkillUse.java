2e27b027fc0b26aaa451adad98755b44de2a5aa4
SomeDays <SomeDays@8c380beb-a0af-4294-b7aa-acb47a1333cf>
2011-11-25 21:19:52 +0000

    Support After Skill Use Player Should Stop Like L2OFF [Need To Be Continue on more skills]

--- branches/CT2.5/FreyaCore/java/l2/brick/gameserver/network/clientpackets/RequestMagicSkillUse.java
+++ branches/CT2.5/FreyaCore/java/l2/brick/gameserver/network/clientpackets/RequestMagicSkillUse.java
@@ -100,6 +100,206 @@ public final class RequestMagicSkillUse extends L2GameClientPacket
                 final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
                 activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
             }
+		
+		    // Stop if use heal-buffs.
+            if(skill.getSkillType() == L2SkillType.HEAL && skill.getTargetType() == SkillTargetType.TARGET_ONE) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use heal percent self buffs. 
+            if(skill.getSkillType() == L2SkillType.HEAL_PERCENT && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use general buffs etc. 
+            if(skill.getSkillType() == L2SkillType.BUFF && skill.getTargetType() == SkillTargetType.TARGET_ONE) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use chant buffs. 
+            if(skill.getSkillType() == L2SkillType.BUFF && skill.getTargetType() == SkillTargetType.TARGET_PARTY) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use servitor buffs. 
+            if(skill.getSkillType() == L2SkillType.BUFF && skill.getTargetType() == SkillTargetType.TARGET_PET) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		 
+		    // Stop if use servitor heal. 
+            if(skill.getSkillType() == L2SkillType.HEAL && skill.getTargetType() == SkillTargetType.TARGET_PET) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use servitor recharge. 
+            if(skill.getSkillType() == L2SkillType.MANARECHARGE && skill.getTargetType() == SkillTargetType.TARGET_PET) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use recharge. 
+            if(skill.getSkillType() == L2SkillType.MANARECHARGE && skill.getTargetType() == SkillTargetType.TARGET_ONE) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use cure poison etc. 
+            if(skill.getSkillType() == L2SkillType.NEGATE && skill.getTargetType() == SkillTargetType.TARGET_ONE) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		  
+		    // Stop if use blessing of eva. 
+            if(skill.getSkillType() == L2SkillType.HPMPHEAL_PERCENT && skill.getTargetType() == SkillTargetType.TARGET_ONE) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use restoration. 
+            if(skill.getSkillType() == L2SkillType.HOT && skill.getTargetType() == SkillTargetType.TARGET_ONE) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use restoration impact. 
+            if(skill.getSkillType() == L2SkillType.HEAL_PERCENT && skill.getTargetType() == SkillTargetType.TARGET_ONE) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use group heal's. 
+            if(skill.getSkillType() == L2SkillType.HEAL && skill.getTargetType() == SkillTargetType.TARGET_PARTY) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use pow/pof/pow. 
+            if(skill.getSkillType() == L2SkillType.BUFF && skill.getTargetType() == SkillTargetType.TARGET_PARTY_MEMBER) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		 
+		    // Stop if use summon servitor. 
+            if(skill.getSkillType() == L2SkillType.SUMMON && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use Self Heal. 
+            if(skill.getSkillType() == L2SkillType.HEAL && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use chant of life etc. 
+            if(skill.getSkillType() == L2SkillType.HOT && skill.getTargetType() == SkillTargetType.TARGET_PARTY) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use braveheart etc. 
+            if(skill.getSkillType() == L2SkillType.COMBATPOINTHEAL && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use bandage etc. 
+            if(skill.getSkillType() == L2SkillType.NEGATE && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use body to mind. 
+            if(skill.getSkillType() == L2SkillType.MANAHEAL && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use cubic party etc. 
+            if(skill.getSkillType() == L2SkillType.SUMMON && skill.getTargetType() == SkillTargetType.TARGET_PARTY) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use summon cp potion. 
+            if(skill.getSkillType() == L2SkillType.CREATE_ITEM && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use symbol of energy etc. 
+            if(skill.getSkillType() == L2SkillType.SIGNET && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use simple cubic etc. 
+            if(skill.getSkillType() == L2SkillType.SUMMON && skill.getTargetType() == SkillTargetType.TARGET_SELF) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
+		
+		    // Stop if use flames of invincibility. 
+            if(skill.getSkillType() == L2SkillType.BUFF && skill.getTargetType() == SkillTargetType.TARGET_PARTY_CLAN) 
+            { 
+                final PcPosition charPos = activeChar.getPosition(); 
+                final L2CharPosition stopPos = new L2CharPosition(charPos.getX(), charPos.getY(), charPos.getZ(), charPos.getHeading()); 
+                activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, stopPos); 
+            }
 		}
 		else
 		{
