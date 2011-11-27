f6bae7e2cdd60cdec05ff3d2aea9bed332ff085d
SomeDays <SomeDays@8c380beb-a0af-4294-b7aa-acb47a1333cf>
2011-11-27 16:29:13 +0000

    Fix over npc talk blockage

--- branches/CT2.5/FreyaCore/java/l2/brick/gameserver/network/serverpackets/NpcHtmlMessage.java
+++ branches/CT2.5/FreyaCore/java/l2/brick/gameserver/network/serverpackets/NpcHtmlMessage.java
@@ -178,10 +178,6 @@ public final class NpcHtmlMessage extends L2GameServerPacket
 	{
 		if (Config.BYPASS_VALIDATION && _validate)
 			buildBypassCache(getClient().getActiveChar());
-		
-		if(Config.PLAYER_MOVEMENT_BLOCK_TIME > 0
-				&& !getClient().getActiveChar().isGM())
-			getClient().getActiveChar().updateNotMoveUntil();
 	}
 	
 	public void setHtml(String text)
